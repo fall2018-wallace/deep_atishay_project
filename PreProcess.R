6aff1db234db83453ac013518cf015358efa66ee
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-02 11:42:42 -0400

    Saving PreProcess.R

--- PreProcess.R
+++ PreProcess.R
@@ -1,6 +1,10 @@
 
 df <- satisfaction_survey
 
+
+df$Satisfaction[df$Satisfaction == "4.00.5" | df$Satisfaction == "4.00.2.00"] = 4
+df$Satisfaction <- as.double(df$Satisfaction)
+
 plot <- hist(df$Satisfaction)
 
 plot
