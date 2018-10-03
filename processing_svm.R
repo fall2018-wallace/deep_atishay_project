56a79ff0f64d58184d6f28b8f3fbd7ff49164108
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-03 10:15:20 -0400

    Saving processing_svm.R

--- processing_svm.R
+++ processing_svm.R
@@ -4,10 +4,10 @@ df <- df
 dfSVM <- df[,c(2:8,10,11,13,23:52)]
 #dfSVM <- df[,c(1:3,4:7,10,12,13,15,25,26,28,29,31:39,40:54)]
 dfSVMTrain <- dfSVM[1:10000,1:40]
-nrow(dfSVMTrain)
+#nrow(dfSVMTrain)
 #c <- dfSVM[10001:12000,53]
 dfSVMTest <- dfSVM[10001:12000,1:40]
-nrow(dfSVMTest)
+#nrow(dfSVMTest)
 #colnames(dfSVMTest)
 library(kernlab)
 
@@ -16,11 +16,7 @@ library(kernlab)
 ksvm_Model <- ksvm(StatisfactionType ~Scheduled.Departure.Hour_Early_Morning+Type.of.Travel_Mileage.tickets+Type.of.Travel_Business.travel+Flight.Distance+Flight.time.in.minutes+Departure.Delay.in.Minutes+Arrival.Delay.in.Minutes+Class+Shopping.Amount.at.Airport+No.of.Flights.p.a.+Year.of.First.Flight+Price.Sensitivity+Age+Gender+Airline.Status , data=dfSVMTrain, kernel = "rbfdot", kpar = "automatic", C = 10, cross = 10, prob.model = TRUE)
 
 
-
-
-
 ksvm_Model
-
 
 ksvmPredict <- predict(ksvm_Model, dfSVMTest)
 length(ksvmPredict)
