98f6b2b1962f477af2e1b35bc166719cf99d1b2e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-03 09:52:12 -0400

    Saving For_RF.R

--- For_RF.R
+++ For_RF.R
@@ -54,22 +54,19 @@ rf_classifier = randomForest(StatisfactionType ~Scheduled.Departure.Hour_Early_M
                                Departure.Delay.in.Minutes+Arrival.Delay.in.Minutes+Class+
                                Shopping.Amount.at.Airport+No.of.Flights.p.a.+Year.of.First.Flight+
                                Price.Sensitivity+Age+Gender+Airline.Status,data=dfRFTrain, ntree=50)
-rf_classifier1 = randomForest(StatisfactionType ~Scheduled.Departure.Hour_Early_Morning+Type.of.Travel_Mileage.tickets+
-                               Type.of.Travel_Business.travel+Flight.Distance+
-                               Arrival.Delay.in.Minutes+Class+
-                               Shopping.Amount.at.Airport+No.of.Flights.p.a.+Year.of.First.Flight+
-                               Price.Sensitivity+Age+Gender+Airline.Status,data=dfRFTrain, ntree=50)
+
 pred_randomForest <- predict(rf_classifier, dfRFTest)
 rfText <- table(c,pred_randomForest)
 accuracy <- (rfText[1]+ rfText[5]+rfText[9])/sum(rfText)
 accuracy
 
-pred_randomForest1 <- predict(rf_classifier1, dfRFTest)
-rfText1 <- table(c,pred_randomForest1)
-accuracy1 <- (rfText1[1]+ rfText1[5]+rfText1[9])/sum(rfText1)
-accuracy1
 
 #plotRFIMP <-varImpPlot(rf_classifier,sort=TRUE)
 
 
 #plotRFIMP
+
+pred_randomForest1 <- predict(rf_classifier1, dfRFTest)
+rfText1 <- table(c,pred_randomForest1)
+accuracy1 <- (rfText1[1]+ rfText1[5]+rfText1[9])/sum(rfText1)
+accuracy1
