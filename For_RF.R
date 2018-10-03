79d2e5572987da3d31c2065776370be81aba27b5
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-03 09:46:16 -0400

    Saving For_RF.R

--- For_RF.R
+++ For_RF.R
@@ -64,6 +64,10 @@ rfText <- table(c,pred_randomForest)
 accuracy <- (rfText[1]+ rfText[5]+rfText[9])/sum(rfText)
 accuracy
 
+pred_randomForest1 <- predict(rf_classifier1, dfRFTest)
+rfText1 <- table(c,pred_randomForest1)
+accuracy <- (rfText1[1]+ rfText1[5]+rfText1[9])/sum(rfText1)
+accuracy1
 plotRFIMP <-varImpPlot(rf_classifier,sort=TRUE)
 
 
