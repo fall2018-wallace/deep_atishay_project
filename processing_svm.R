36fb547172e0584dfefe9398e65d8c0513fead40
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-03 07:13:38 -0400

    Saving processing_svm.R

--- processing_svm.R
+++ processing_svm.R
@@ -1,1 +1,40 @@
 
+dfSVM <- df[,c(1:3,5:8,10,12,13,15,25,26,28,29,31:39,40:54)]
+dfSVMTrain <- dfSVM[1:10000,2:39]
+
+c <- dfSVM[10001:12000,53]
+dfSVMTest <- dfSVM[10001:12000,2:39]
+
+library(kernlab)
+
+#ksvm_Model <- ksvm(StatisfactionType ~Scheduled.Departure.Hour_Early_Morning,data=dfSVMTrain, kernel = "rbfdot", kpar = "automatic", C = 10, cross = 1, prob.model = TRUE)
+
+ksvm_Model <- ksvm(StatisfactionType ~Scheduled.Departure.Hour_Early_Morning+Type.of.Travel_Mileage.tickets+
+                     Type.of.Travel_Business.travel+Flight.Distance+Flight.time.in.minutes+
+                     Departure.Delay.in.Minutes+Arrival.Delay.in.Minutes+Class+
+                     Shopping.Amount.at.Airport+No.of.Flights.p.a.+Year.of.First.Flight+
+                     Price.Sensitivity+Age+Gender+Airline.Status,data=dfSVMTrain, kernel = "rbfdot", kpar = "automatic", C = 10, cross = 10, prob.model = TRUE)
+
+
+
+
+
+ksvm_Model
+
+
+ksvmPredict <- predict(ksvm_Model, dfSVMTest)
+length(ksvmPredict)
+nrow(dfSVMTest)
+
+dfSVMTest$predictedValues <- ksvmPredict
+
+
+#Function to check accuracy of predicted model
+functionForKSVMAccuracy <- function(dataSet){
+  a <- ifelse(dataSet$dfSVMTest.StatisfactionType == dataSet$dfSVMTest.predictedValues, 1,0)
+  return(length(which((a==1) == TRUE))/nrow(dataSet) * 100)
+}
+
+comparisonTable <- data.frame(dfSVMTest$StatisfactionType,dfSVMTest$predictedValues)
+table(comparisonTable)
+functionForKSVMAccuracy(comparisonTable)
