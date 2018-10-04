4e04b6d3d4c69e5c3eaec8f7fa46763cbb9cfb5f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 10:24:05 -0400

    Saving RF_testing.R

--- RF_testing.R
+++ RF_testing.R
@@ -1,5 +1,6 @@
 
 rf_classifier <- rf_classifier
+
 pred_randomForest <- predict(rf_classifier, dfRFTest)
 rfText <- table(c,pred_randomForest)
 accuracy <- (rfText[1]+ rfText[5]+rfText[9])/sum(rfText)
