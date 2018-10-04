01ed8b425af931e823345d9c525b926f4b2661e2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 10:34:03 -0400

    Saving For_RF.R

--- For_RF.R
+++ For_RF.R
@@ -8,13 +8,6 @@ dfRFTrain <- dfRF[1:100000,1:40]
 c <- dfRF[100001:129889,40]
 dfRFTest <- dfRF[100001:129889,1:39]
 
-rf_classifier = randomForest(StatisfactionType ~Scheduled.Departure.Hour_Early_Morning+Type.of.Travel_Mileage.tickets+
-                               Type.of.Travel_Business.travel+Flight.Distance+Flight.time.in.minutes+
-                               Departure.Delay.in.Minutes+Arrival.Delay.in.Minutes+Class+
-                               Shopping.Amount.at.Airport+No.of.Flights.p.a.+Year.of.First.Flight+
-                               Price.Sensitivity+Age+Gender+Airline.Status,data=dfRFTrain, ntree=50)
-rf_classifier
-
 
 rf_classifier1 = randomForest(StatisfactionType ~Scheduled.Departure.Hour_Early_Morning+Type.of.Travel_Mileage.tickets+
                                Type.of.Travel_Business.travel+Flight.Distance+
