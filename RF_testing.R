8e39cef80bf4f38944fd4b02d9e5a46e8eb64413
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 10:32:33 -0400

    Saving RF_testing.R

--- RF_testing.R
+++ RF_testing.R
@@ -1,14 +1,4 @@
 
-library(randomForest)
-
-rf_classifier <- rf_classifier
-
-pred_randomForest <- predict(rf_classifier, dfRFTest)
-rfText <- table(c,pred_randomForest)
-accuracy <- (rfText[1]+ rfText[5]+rfText[9])/sum(rfText)
-accuracy
-
-
 plotRFIMP <-varImpPlot(rf_classifier,sort=TRUE)
 
 
